1726ee98d1299b724e8b56cc82a9dab106bcea06
Alexander Ramos <alexander.br159@gmail.com>
2021-05-16 13:39:19 +0200

    Refactorizacion Realizada

---
+++ Vivienda/src/Hogar/Hogar.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hogar;
+
+/**
+ *
+ * @author lucasjosemoraleschacon
+ */
+public class Hogar 
+{
+
+    
+    public static void main(String[] args) 
+    {
+        Vivienda casa1 = new Vivienda(120000);
+        
+        double precio= 60000.0;
+        int numHabitaciones=3;
+        double superficie=200.0;
+        boolean parking=true;
+        String estado= "Bueno";
+        String propietario= "Pedro";
+        
+    }
+    
+    public static void imprimirVivienda(Vivienda casa1){
+    	System.out.println("Precio: " + casa1.getPrecio());
+        System.out.println("Número de habitaciones: "+ casa1.getNumHabitaciones());
+        System.out.println("Superficie en m2: " + casa1.getSuperficie());
+        System.out.println("¿Dispone de parking?: " + casa1.isParking());
+        System.out.println("Estado: "+ casa1.getEstado());
+        System.out.println("Propietario: "+ casa1.getPropietario());
+    }
+    
+}
---
+++ Vivienda/src/Hogar/Vivienda.java
@@ -0,0 +1,113 @@
+
+package Hogar;
+
+
+public class Vivienda 
+{
+	private double precio;
+    private  int numHabitaciones;
+    private double superficie;
+    private boolean parking;
+    private String estado;
+    private String propietario;
+   
+    /*-------------------------------------------------*/
+    /**
+	 * @param precio
+	 * @param numHabitaciones
+	 * @param superficie
+	 * @param parking
+	 * @param estado
+	 * @param propietario
+	 */
+	public Vivienda(double precio, int numHabitaciones, double superficie, boolean parking, String estado,
+			String propietario) {
+		super();
+		this.precio = precio;
+		this.numHabitaciones = numHabitaciones;
+		this.superficie = superficie;
+		this.parking = parking;
+		this.estado = estado;
+		this.propietario = propietario;
+	}
+
+	/*----------------------------------------------*/
+    public double getPrecio() {
+        return precio;
+    }
+    /**
+     * 
+     * @param precio 
+     */
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getNumHabitaciones() {
+        return numHabitaciones;
+    }
+    /**
+     * 
+     * @param numHabitaciones 
+     */
+    public void setNumHabitaciones(int numHabitaciones) {
+        this.numHabitaciones = numHabitaciones;
+    }
+
+    public double getSuperficie() {
+        return superficie;
+    }
+    /**
+     * 
+     * @param superficie 
+     */
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+    }
+
+    public boolean isParking() {
+        return parking;
+    }
+    /**
+     * 
+     * @param parking 
+     */
+    public void setParking(boolean parking) {
+        this.parking = parking;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+    /**
+     * 
+     * @param estado 
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPropietario() {
+        return propietario;
+    }
+    /**
+     * 
+     * @param propietario 
+     */
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+    /*---------------------------------------------------*/
+    public Vivienda (double precio)
+    {
+        this.precio = precio;
+    }
+    
+    public void actualizarPrecio(double descuento)
+    {
+        precio = precio - precio *(descuento/0.05);
+    }
+    
+    
+    
+}
